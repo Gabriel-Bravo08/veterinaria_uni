94c0c0c852464fb7c22b119cb6448aeb5878892f
Gabriel-Bravo08 <bravog0898@gmail.com>
2025-08-11 08:44:06 -0600

    Refactor

---
+++ src/app/pages/components/principal-page/principal-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-page',
+  imports: [],
+  templateUrl: './principal-page.component.html',
+  styleUrl: './principal-page.component.scss'
+})
+export class PrincipalPageComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,12 +3,23 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
 import { CitasComponent } from '../components/citas/citas.component';
 import { CarruselComponent } from '../components/carrusel/carrusel.component';
-import { ContactComponent } from '../../shared/contact/contact.component';
+import { ContactComponent } from '../components/contact/contact.component';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from "../components/footer/footer.component";
+import { ServicesComponent } from "../components/services/services.component";
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule, CommonModule, SidebarComponent, CitasComponent, CarruselComponent, ContactComponent],
+  imports: [
+    FormsModule,
+    CommonModule,
+    SidebarComponent,
+    CitasComponent,
+    CarruselComponent,
+    ContactComponent,
+    FooterComponent,
+    ServicesComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
